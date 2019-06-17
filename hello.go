41917ca824cb07d9c1636ad9e10a35bcb9831632
farizattamimi <attamimi@usc.edu>
2019-06-17 19:50:37 +0700

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("hello, world\n")
+	x := 1
+	s := "hello"
+	fmt.Printf("%s I am %d\n", s, x)
+}
---
+++ variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a = "fariz"
+	fmt.Println(a)
+
+	b, c := 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+    fmt.Println(d)
+	
+}
+
