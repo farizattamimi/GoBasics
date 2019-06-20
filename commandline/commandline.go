3503323a5953ff10ed45f62af7d7a221ac8af627
farizattamimi <attamimi@usc.edu>
2019-06-20 16:22:32 +0700

    Add files via upload

---
+++ commandline/commandline.go
@@ -0,0 +1,17 @@
+package main
+
+import "os"
+import "fmt"
+import "strconv"
+
+func main() {
+
+    argsWithProg := os.Args
+    argsWithoutProg := os.Args[1:]
+
+    arg := os.Args[3]
+
+    fmt.Println(argsWithProg)
+    fmt.Println(argsWithoutProg)
+    fmt.Println(arg)
+}
